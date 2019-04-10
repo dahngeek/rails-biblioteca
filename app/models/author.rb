04e3f67dd05dda5a3ec6831acd8560835926bdd4
Daniel Xutuc <danielxutuc@gmail.com>
2019-04-10 17:14:14 -0300

    selector collection de nombres de los autores

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,11 @@
 class Author < ApplicationRecord
     has_many :books
+    
     def bookpages
         Book.where(:author_id => self.id).sum(:pages)
+    end
+
+    def full_name
+        self.name + " " + self.lastname
     end
 end
